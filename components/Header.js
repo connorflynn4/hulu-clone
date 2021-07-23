3f73c99db0045c350c7fe37785e54e5bc8460b70
Connor Flynn <connorflynn4@gmail.com>
2021-07-22 21:17:57 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,15 +5,15 @@ import {
     LightningBoltIcon,
     SearchIcon,
     UserIcon,
-}from "@heroicons/react/outline";
+} from "@heroicons/react/outline";
 
-import Image from "next/image"
-import HeaderItem from "./HeaderItem"
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
 
 function Header() {
     return (
         <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
-            <div className = "flex flex-grow justify-evenly max-w-2xl">
+            <div className="flex flex-grow justify-evenly max-w-2xl">
                 <HeaderItem title="HOME" Icon={HomeIcon} />
                 <HeaderItem title="TRENDING" Icon={LightningBoltIcon} /> 
                 <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} /> 
@@ -21,11 +21,10 @@ function Header() {
                 <HeaderItem title="SEARCH" Icon={SearchIcon} /> 
                 <HeaderItem title="ACCOUNT" Icon={UserIcon} />  
             </div>
-            <Image className="object-contain"
-            src="https://links.papareact.com/ua6"
-            width={200}
-            height={100}
-            />
+            <Image
+                className="object-contain"
+                src="https://links.papareact.com/ua6"
+                width={200} height={100} />
         </header>
     )
 }
